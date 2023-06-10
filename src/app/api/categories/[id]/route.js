bed3d11cc74313f9395fae75802be90181970bf0
Thuong VNBK <congthuong@vietnambooking.com>
2023-06-10 08:51:55 +0700

    evisa

--- src/app/api/categories/[id]/route.js
+++ src/app/api/categories/[id]/route.js
@@ -1,49 +1,49 @@
-import { NextResponse } from "next/server";
-import Category from "@/models/Category";
+import { NextResponse } from 'next/server';
+import Category from '@/models/Category';
 
 export const PUT = async (request, { params }) => {
-  const { id } = params;
-  const dataUpdate = await request.json();
+    const { id } = params;
+    const dataUpdate = await request.json();
 
-  try {
-    const result = await Category.update(dataUpdate, {
-      where: {
-        id
-      },
-    });
+    try {
+        const result = await Category.update(dataUpdate, {
+            where: {
+                id,
+            },
+        });
 
-    return new NextResponse("Category has been updated", { status: 200 });
-  } catch (err) {
-    return new NextResponse("Database Error", { status: 500 });
-  }
+        return new NextResponse('Category has been updated', { status: 200 });
+    } catch (err) {
+        return new NextResponse('Database Error', { status: 500 });
+    }
 };
 
 export const GET = async (request, { params }) => {
-  const { id } = params;
-  try {
-    const result = await Category.findOne({
-      where: {
-        id
-      },
-    });
-    return new NextResponse(JSON.stringify(result), { status: 200 });
-  } catch (err) {
-    return new NextResponse("Database Error", { status: 500 });
-  }
+    const { id } = params;
+    try {
+        const result = await Category.findOne({
+            where: {
+                id,
+            },
+        });
+        return new NextResponse(JSON.stringify(result), { status: 200 });
+    } catch (err) {
+        return new NextResponse('Database Error', { status: 500 });
+    }
 };
 
 export const DELETE = async (request, { params }) => {
-  const { id } = params;
+    const { id } = params;
 
-  try {
-    const result = await Category.destroy({
-      where: {
-        id
-      },
-    });
+    try {
+        const result = await Category.destroy({
+            where: {
+                id,
+            },
+        });
 
-    return new NextResponse("Category has been deleted", { status: 200 });
-  } catch (err) {
-    return new NextResponse("Database Error", { status: 500 });
-  }
+        return new NextResponse('Category has been deleted', { status: 200 });
+    } catch (err) {
+        return new NextResponse('Database Error', { status: 500 });
+    }
 };
--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,36 +1,36 @@
-import { NextResponse } from "next/server";
-import Post from "@/models/Post";
+import { NextResponse } from 'next/server';
+import Post from '@/models/Post';
 
 export const GET = async (request) => {
-  // const url = new URL(request.url);
-  // const id = url.searchParams.get("id");
-  try {
-    const result = await Post.findAll();
-    return new NextResponse(JSON.stringify(result), { status: 200 });
-  } catch (err) {
-    return new NextResponse("Database Error", { status: 500 });
-  }
+    // const url = new URL(request.url);
+    // const id = url.searchParams.get("id");
+    try {
+        const result = await Post.findAll();
+        return new NextResponse(JSON.stringify(result), { status: 200 });
+    } catch (err) {
+        return new NextResponse('Database Error', { status: 500 });
+    }
 };
 
 export const POST = async (request) => {
-  const { title, meta_desc, meta_title, image, content, slug, author, updated_by, status } = await request.json();
-  //  const body = await request.json();
-   console.log(body);
-  try {
-    const newPost = await Post.create({
-      title,
-      meta_desc,
-      meta_title,
-      image,
-      content,
-      slug,
-      author,
-      updated_by,
-      status
-    });
+    const { title, meta_desc, meta_title, image, content, slug, author, updated_by, status } = await request.json();
+    //  const body = await request.json();
+    console.log(body);
+    try {
+        const newPost = await Post.create({
+            title,
+            meta_desc,
+            meta_title,
+            image,
+            content,
+            slug,
+            author,
+            updated_by,
+            status,
+        });
 
-      return new NextResponse("Post has been created", { status: 201 });
-  } catch (error) {
-    return new NextResponse("Database Error", { status: 500 });
-  }
+        return new NextResponse('Post has been created', { status: 201 });
+    } catch (error) {
+        return new NextResponse('Database Error', { status: 500 });
+    }
 };
