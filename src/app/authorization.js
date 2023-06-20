fa9b9189f5c5f490b29365adf5ff5f91ecda909c
Thuong VNBK <congthuong@vietnambooking.com>
2023-06-20 10:08:38 +0700

    evisa

--- src/app/authorization.js
+++ src/app/authorization.js
@@ -5,11 +5,13 @@ export async function isAllowed(request, checkRole = null) {
     const decodedPayload = JSON.parse(payload);
 
     if (!decodedPayload) return false;
-
-    const user = await User.findOne({
-        email: decodedPayload.email,
+    const where = {
+        where: {
+            email: decodedPayload.email,
+        },
         raw: true,
-    });
+    };
+    const user = await checkUser(where);
 
     if (!user || user.role !== decodedPayload.role) return false;
     if (checkRole && user.role !== checkRole) return false;
@@ -24,4 +26,11 @@ export async function isAllowed(request, checkRole = null) {
 
 export function isAdmin(request) {
     return isAllowed(request, 'administrator');
+}
+
+export async function checkUser(where, returnObj = true) {
+    const user = await User.findOne(where);
+    if (!user) return false;
+    if (returnObj) return user;
+    return true;
 }
